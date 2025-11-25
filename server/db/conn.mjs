57b48a9c202e957c32ab1b8f1e63dd7d2573acc9
DanTurekCIS2454 <turek.daniel@student.oaklandcc.edu>
2025-11-24 22:00:55 -0500

    server working
    Combined with latest mongoDb start

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+/* import { MongoClient } from "mongodb";
 
 const connectionString = process.env.ATLAS_URI || "";
 
@@ -10,6 +10,35 @@ try {
 } catch(e) {
   console.error(e);
 }
+
+let db = conn.db("sample_training");
+
+export default db;
+*/
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+//const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.ATLAS_URI || "";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+let conn
+try {
+    // Connect the client to the server	(optional starting in v4.7)
+    conn = await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch(e) {
+    console.error(e);
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 
 let db = conn.db("sample_training");
 
